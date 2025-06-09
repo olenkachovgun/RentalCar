cf5ff22380b17a19f93a35db5be608edf8eb2503
Olena_Chovgun <olenkachovgun@gmail.com>
2025-06-09 23:36:02 +0300

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import clsx from "clsx";
+import { NavLink } from "react-router-dom";
+import s from "./Header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
-}
+  return (
+    <header className={s.header}>
+      <NavLink to="/">
+        <img
+          src="/public/rent-car-logo.svg"
+          alt="RentalCar Logo"
+          className={s.logo}
+        />
+      </NavLink>
+      <nav className={s.navHeader}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => clsx(s.link, isActive && s.active)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) => clsx(s.link, isActive && s.active)}
+        >
+          Catalog
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
