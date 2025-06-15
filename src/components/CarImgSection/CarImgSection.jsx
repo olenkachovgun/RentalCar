536950696e9e5cd2f74dd4844d2420ea4f663ed3
Olena_Chovgun <olenkachovgun@gmail.com>
2025-06-15 23:51:55 +0300

    fix css for adaptive

--- src/components/CarImgSection/CarImgSection.jsx
+++ src/components/CarImgSection/CarImgSection.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import s from "./CarImgSection.module.css";
 
 const CarImgSection = ({ img, altText }) => {
-  return (
-    <>
-      <img src={img} alt={altText} className={s.carImg} />
-    </>
-  );
+  return <img src={img} alt={altText} className={s.carImg} />;
 };
 
 export default CarImgSection;
