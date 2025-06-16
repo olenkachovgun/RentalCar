65c43de79ba17aa0dd3ffaefbf45f9f316f3086b
Olena Chovgun <59543079+olenkachovgun@users.noreply.github.com>
2025-06-16 21:12:26 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,11 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { carsReducer } from "./redux/cars/slice.js";
 
-// import { filtersReducer } from "./filtersSlice";
-
-export const store = configureStore({
-  reducer: {
-    cars: carsReducer,
-    // filters: filtersReducer,
-  },
-});
